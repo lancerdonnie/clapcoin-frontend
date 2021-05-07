cc28bc7822db08bcd295d64d60f568fbf5ca7016
lancerdonnie <jideadedejifirst@gmail.com>
2021-05-07 13:36:29 +0100

    removed unused import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { FooterIcons } from 'components/FooterIcons';
 import { Navbar } from 'components/Navbar';
-import { Twitter } from 'components/Svg';
 import { Values } from 'components/Values';
 import Head from 'next/head';
 import styles from 'styles/Home.module.css';
