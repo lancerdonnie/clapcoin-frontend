dadddd8a04cee4c2629d94f7e3f1e9f7a248ea16
lancerdonnie <jideadedejifirst@gmail.com>
2021-05-14 19:40:14 +0100

    added register page

--- pages/register.js
+++ pages/register.js
@@ -4,16 +4,17 @@ import * as yup from 'yup';
 import { Input } from 'components/Form/Input';
 import ReCAPTCHA from 'react-google-recaptcha';
 import { RECAPATCHA_SITE_KEY } from 'lib/constants';
-import Link from 'next/link';
 
 const resolver = yupResolver(
   yup.object().shape({
+    username: yup.string().required(),
     email: yup.string().required(),
     password: yup.string().min(8).required(),
+    confirmpassword: yup.string().min(8).required(),
   })
 );
 
-export default function login() {
+export default function register() {
   const {
     register,
     handleSubmit,
@@ -28,7 +29,13 @@ export default function login() {
         onSubmit={handleSubmit(onSubmit)}
         className="w-1/2 p-8 px-16 md:w-full sm:px-8"
       >
-        <div className="my-8">Login to your account</div>
+        <div className="my-8">Create your account</div>
+        <Input
+          name="username"
+          label="Username"
+          register={register}
+          errors={errors}
+        />
         <Input name="email" label="Email" register={register} errors={errors} />
         <Input
           name="password"
@@ -37,9 +44,13 @@ export default function login() {
           errors={errors}
           type="password"
         />
-        <a className="block text-right text-xs text-yellow-500" href="">
-          Forgot password?
-        </a>
+        <Input
+          name="confirmpassword"
+          label="Confirm Password"
+          register={register}
+          errors={errors}
+          type="password"
+        />
         <div className="mt-2 flex justify-center">
           <ReCAPTCHA
             sitekey={RECAPATCHA_SITE_KEY}
@@ -48,18 +59,18 @@ export default function login() {
             }}
           />
         </div>
+        <div className="block text-center text-xs mt-4">
+          By clicking the button below, you agree to ClapCoin{' '}
+          <a className="text-blue-400" href="">
+            Privacy Policy
+          </a>
+        </div>
         <button
           className="mt-4 rounded block bg-accent-2 text-white w-full px-4 py-2"
           type="submit"
         >
-          Login
+          Register
         </button>
-        <div className="block text-center text-xs mt-4">
-          Don't have an account?{' '}
-          <Link href="/register">
-            <a className="font-bold">Create an account</a>
-          </Link>
-        </div>
       </form>
       <div className="bg-nav flex-1 flex justify-center items-center md:hidden">
         <img src="logo.png" className="" />
